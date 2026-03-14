c2e1259105deef7921b438c194543ae14b6f2c3c
shihab97820 <127777144+shihab97820@users.noreply.github.com>
2026-03-14 16:26:10 +0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+# Simple Python program for GitHub repository
+
+print("Hello, GitHub!")
+
+name = input("What is your name? ")
+print("Welcome to my GitHub project,", name)
+
+# simple calculation
+a = 10
+b = 5
+sum = a + b
+
+print("The sum of", a, "and", b, "is", sum)
